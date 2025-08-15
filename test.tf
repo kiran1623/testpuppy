b4a7c5a98acda0ce200bbe54416e5e8a5a4edb99
Your Name <you@example.com>
2025-08-15 13:51:27 +0530

    space added

---
+++ test.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.21.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "c30a9880-60c2-4613-b1ed-fd523ed22583"
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-test-01"
+  location = "westus"
+}
+
